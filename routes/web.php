b7cab4b45210e2163799f8e7970601b427e5467f
Erickittie <dumalaganjererick@gmail.com>
2026-07-08 20:17:07 +0800

    add routes for landing and restration page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'landing');
+
+Route::view('/registration', 'auth.registration');
+
